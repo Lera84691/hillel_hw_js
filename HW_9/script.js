8c6ed05e37fb935af133c6e9dc2c94f250f5dc84
valery <valery.kuznecovaa@gmail.com>
2022-02-17 10:18:36 +0200

    adedd a task

--- HW_9/script.js
+++ HW_9/script.js
@@ -1,13 +1,12 @@
 //Написать функцию, которая принимает один параметр. При первом вызове, она его запоминает, при втором - суммирует переданный параметр с тем, что передали первый раз и так далее. Всё это с замыканиями. Например:
-
-// a.sum(3) = 3
-// b.3sum(5) = 8
-// c.sum(20) = 28
+//a. sum(3) -> ф-ция возвращает результат 3
+//b. sum(5) -> ф-ция возвращает результат 8
+//c. sum(20) -> ф-ция возвращает результат 28
 
 (function () {
-    function sum (val) {
+    function sum(val) {
         let temp = val;
-        sum = function() {
+        sum = function () {
             let arg = arguments[0] ? arguments[0] : 0;
             return temp = temp + arg;
         };
@@ -18,17 +17,4 @@
     console.log(sum(28)); //39
 })();
 
-// или
 
-(function () {
-    function sum (val) {
-        let temp = 0;
-        sum = function(arg) {
-            return temp = temp + arg;
-        };
-        return sum(val);
-    }
-    console.log(sum(3)); //3
-    console.log(sum(8)); //11
-    console.log(sum(28)); //39
-})();
