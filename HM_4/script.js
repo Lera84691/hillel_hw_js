f59293e337d120525788895e6385d77135f44558
valery <valery.kuznecovaa@gmail.com>
2022-02-17 09:59:47 +0200

    added changes to the game

--- HM_4/script.js
+++ HM_4/script.js
@@ -5,8 +5,8 @@ let num = '';
 while (a <= 20) {
   num += a + ', ';
   a++
-}
-num += a
+};
+num += a;
 console.log(num);
 
 
@@ -14,21 +14,21 @@ console.log(num);
 
 for (let b = 10; b < 21; b++) {
   console.log("Квадрат " + b + " = " + b * b);
-}
+};
 
 
 //3.Вывести таблицу умножения на 7
 let d = 7
 for (let c = 1; c <= 9; c++) {
   console.log(d + "*" + c + "=" + (d * c));
-}
+};
 
 
 //4.Найти сумму всех целых чисел от 1 до 15
 let sum = 0;
 for (let e = 1; e <= 15; e++) {
   sum += e;
-}
+};
 console.log(sum);
 
 
@@ -37,7 +37,7 @@ console.log(sum);
 let mult = 1;
 for (let f = 15; f <= 35; f++) {
   mult *= f;
-}
+};
 console.log(mult);
 
 
@@ -46,7 +46,7 @@ let average = 0;
 let g = 500;
 for (g = 1; g <= 500; g++) {
   average += g;
-}
+};
 console.log(average / 500);
 
 
@@ -55,16 +55,16 @@ var h = 0;
 for (var i = 30; i <= 80; i++) {
   if (i % 2 == 0)
     h += i;
-}
-console.log(h)
+};
+console.log(h);
 
 
 //8. Вывести на страницу все числа в диапазоне от 100 до 200 кратные 3
 let k = 200;
 for (let j = 100; j < k; j++) {
-  if (j % 3 === 0)
+  if (j % 3 === 0);
     console.log(j);
-}
+};
 
 //9. Дано натуральное число. Найти и вывести на страницу все его делители. l m n
 //- oпределить количество его четных делителей
@@ -80,7 +80,7 @@ for (let divisor = 2; divisor <= n / 2; divisor++) {
       even += divisor;
     }
   }
-}
+};
 
 console.log(even);
 console.log(total);
@@ -92,10 +92,10 @@ let result = ' x   ';
 function buff(val) {
   let buff = '';
   let pad = 4 - val;
-  while (pad-- > 0)
+  while (pad-- > 0);
     buff += ' ';
   return buff;
-}
+};
 
 for (var p = 0; p < 11; p++) {
   for (var q = 0; q < 11; q++) {
@@ -110,30 +110,29 @@ for (var p = 0; p < 11; p++) {
     }
   }
   result += '\n'
-}
-console.log(result)
+};
+console.log(result);
 
 
 //11.Игра “Угадай число”. Сгенерировать случайное число в диапазоне от 0 до 10. Пользователь должен угадать число. Игра продолжается до тех пор, пока пользователь не угадает число. Пользователь может остановить игру в любой момент.
 let r = Math.floor(Math.random() * 11);
-console.log(r);
+console.log("Случайное число: ", r);
 while (true) {
-  let s = +prompt("Я загадал число между 0 и 10. Сможешь угадать?");
+  let s = prompt("Я загадал число между 0 и 10. Сможешь угадать?");
   if (!s) {
     alert("Приходи играть еще");
     break;
-  } if (isNaN(r)) {
+  } else if (isNaN(s)) {
     alert("Ошибка ввода");
-  } else if (s < r) {
-    alert("Твое число меньше того, что я загадал.");
-  } else if (s > r) {
-    alert("Твое число больше загаданного мной");
+  } else if (+s < r) {
+    alert("Твое число меньше того, что я загадал!");
+  } else if (+s > r) {
+    alert("Твое число больше того, что я загадал!");
   } else {
     alert("Ух ты! Ты угадал мое число!");
     break;
   }
-}
-
+};
 
 //12. Запросить у пользователя число, которое будет высотой вершины прямоугольного треугольника. Вывести треугольник построчно через console.log
 let t = 0;
@@ -144,7 +143,8 @@ while (t < height) {
   for (let u = 0; u < t + 1; u++) trian += "*";
   console.log(trian);
   t++;
-} 
+};
+
 
 
 
