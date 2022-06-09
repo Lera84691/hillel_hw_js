600806b74dbab3192ea16cd6304a70f63035c3b9
valery <valery.kuznecovaa@gmail.com>
2022-06-09 18:15:34 +0300

    added files

---
+++ HW_11/script.js
@@ -0,0 +1,24 @@
+
+function createTable(rowCount, colCount) {
+    const table = document.createElement("table");
+    table.className = "table";
+    let counter = 1;
+  
+    for (let i = 0; i < rowCount; i++) {
+      const tr = document.createElement("tr");
+      tr.classList.add("table-row");
+      table.append(tr);
+  
+      for (let j = 0; j < colCount; j++) {
+        const td = document.createElement("td");
+        td.classList.add("table-cell");
+        td.innerHTML = +counter;
+        tr.append(td);
+        counter++;
+      }
+    }
+   
+    return table;
+  }
+  
+  document.body.append(createTable(10,10));
